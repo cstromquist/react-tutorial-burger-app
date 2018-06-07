3b6c76d179af2aeb39099a6261a9a17d37d5e4a6
Chris Stromquist <chris.stromquist@gmail.com>
2018-06-07 10:33:26 -0700

    Added modal backdrop and purchase cancel.

--- src/controllers/BurgerBuilder/BurgerBuilder.js
+++ src/controllers/BurgerBuilder/BurgerBuilder.js
@@ -30,6 +30,10 @@ class BurgerBuilder extends Component {
 		this.setState({purchasing: true});
 	}
 
+	purchaseCancelHandler = () => {
+		this.setState({purchasing: false});
+	}
+
 	updatePurchaseState (ingredients) {
 		const sum = Object.keys(ingredients)
 			.map(igKey => {
@@ -78,7 +82,7 @@ class BurgerBuilder extends Component {
 		
 		return (
 			<Aux>
-				<Modal show={this.state.purchasing}>
+				<Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
 					<OrderSummary ingredients={this.state.ingredients} />
 				</Modal>
 				<Burger ingredients={this.state.ingredients} />
