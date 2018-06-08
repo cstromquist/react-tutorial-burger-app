de3a3083bcff79125edb509191d569ae3d1063a5
Chris Stromquist <chris.stromquist@gmail.com>
2018-06-08 11:25:58 -0700

    Section 10, Lecture 173 - Removing old interceptors

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './controllers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Layout>
+	render() {
+		return (
+			<div>
+				<Layout>
 					<BurgerBuilder />
 				</Layout>
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 export default App;
