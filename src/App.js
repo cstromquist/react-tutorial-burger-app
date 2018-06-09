0c5365b2bde52997af0d1d3f27d75dc89ee9e91c
Chris Stromquist <chris.stromquist@gmail.com>
2018-06-09 14:07:22 -0700

    Section 12, Lecture 211 - Adding routing and checkout.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import Layout from './hoc/Layout/Layout';
-import BurgerBuilder from './controllers/BurgerBuilder/BurgerBuilder';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<Layout>
-					<BurgerBuilder />
-				</Layout>
-			</div>
+				<div>
+					<Layout>
+						<Switch>
+							<Route path="/checkout" component={Checkout} />
+							<Route path="/" exact component={BurgerBuilder} />
+						</Switch>
+					</Layout>
+				</div>
 		);
 	}
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -45,28 +45,36 @@ class BurgerBuilder extends Component {
 	}
 
 	purchaseContinueHandler = () => {
-		this.setState({ loading: true });
-		const order = {
-			ingredients: this.state.ingredients,
-			price: this.state.totalPrice,
-			customer: {
-				name: 'Jack Johnson',
-				address: {
-					street: '123 Test Ave',
-					zipCode: '12345',
-					country: 'US'
-				},
-				email: 'test@test.com'
-			},
-			deliveryMethod: 'fastest'
+		// this.setState({ loading: true });
+		// const order = {
+		// 	ingredients: this.state.ingredients,
+		// 	price: this.state.totalPrice,
+		// 	customer: {
+		// 		name: 'Jack Johnson',
+		// 		address: {
+		// 			street: '123 Test Ave',
+		// 			zipCode: '12345',
+		// 			country: 'US'
+		// 		},
+		// 		email: 'test@test.com'
+		// 	},
+		// 	deliveryMethod: 'fastest'
+		// }
+		// axios.post('/orders', order)
+		// 	.then(response => {
+		// 		this.setState({ loading: false, purchasing: false });
+		// 	})
+		// 	.catch(error => {
+		// 		this.setState({ loading: false, purchasing: false });
+		// 	});
+		const queryParams = [];
+		for (let i in this.state.ingredients) {
+			queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
 		}
-		axios.post('/orders', order)
-			.then(response => {
-				this.setState({ loading: false, purchasing: false });
-			})
-			.catch(error => {
-				this.setState({ loading: false, purchasing: false });
-			});
+		this.props.history.push({
+			pathname: '/checkout',
+			search: queryParams.join('&')
+		});
 	}
 
 	updatePurchaseState(ingredients) {
